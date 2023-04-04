b7b328680a85f4bda702742dab7dbd6ce2ca9bee
Jake Johnston <jakejohnston618@gmail.com>
2023-04-04 11:50:45 -0500

    finished fizzbuzz

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace FizzBuzz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        public static string FizzBuzz(int number)
+        {
+
+            var answer = "";
+
+            if (number % 3 == 0)
+            {
+                answer = "fizz";
+            }
+            if (number % 5 == 0)
+            {
+                answer += "buzz";
+            }
+
+            return answer;
+
+        }
+    }
+}
